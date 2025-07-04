ae0b323fa3296841382cdfbadb2d458ce25404a4
CugliariSanti17 <cugliarisantiago@gmail.com>
2025-07-04 12:30:48 -0300

    add validation to price field

--- api/src/routes/products.routes.js
+++ api/src/routes/products.routes.js
@@ -6,7 +6,7 @@ const router = Router()
 
 const rules = [
     body('nombre').escape().notEmpty().withMessage('El nombre es obligatorio').bail().isLength({min: 3}).withMessage('El nombre debe tener al menos 3 caracteres'),
-    body('precio').escape().notEmpty().withMessage('El precio es obligatorio').bail().isNumeric().withMessage('El precio debe ser un número'),
+    body('precio').escape().notEmpty().withMessage('El precio es obligatorio').bail().isNumeric().withMessage('El precio debe ser un número').bail().isFloat({min: 0.00001}).withMessage('El precio debe ser positivo o mayor a 0'),
     body('descripcion').escape().notEmpty().withMessage('La descripcion es obligatorio').bail().isLength({min: 10}).withMessage('La descripcion debe tener al menos 10 caracteres')
 ]
 
