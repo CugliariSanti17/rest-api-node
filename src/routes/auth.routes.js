a0ef0d764bb5622243dba46e466f68a92f1c1710
CugliariSanti17 <cugliarisantiago@gmail.com>
2025-07-19 13:46:17 -0300

    fix problems with rules of express-validator and add strings methods in auth.controller

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,7 @@ import { body } from 'express-validator';
 const router = Router();
 
 const rules = [
-    body('email').escape().notEmpty().withMessage('El email es obligatorio.').bail().isEmail().withMessage('El campo email no es un email registrado').normalizeEmail(),
+    body('email').escape().notEmpty().withMessage('El email es obligatorio.').bail().isEmail().withMessage('El campo email no es un email registrado'),
     body('password').escape().notEmpty().withMessage('La contraseña es obligatoria')
 ]
 
